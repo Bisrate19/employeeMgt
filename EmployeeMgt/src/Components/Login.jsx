21bfc76775a7ca0e1e4b0a96bbcc1bc0dfe973a0
Bisrat Tamire <biarattamire288@gmail.com>
2025-02-12 21:07:00 +0300

    inputs

--- EmployeeMgt/src/Components/Login.jsx
+++ EmployeeMgt/src/Components/Login.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Components/style.css"
+import axios from 'axios'
 const Login = () => {
+
+const [values, setValues] = useState({
+    email: '',
+    password: ""
+})
+
+const handleSubmit =(event) =>{
+    event.preventDefault()
+    axios.post('http://localhost:3000/auth/adminlogin')
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+}
+
   return (
     <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
         <div className='p-3 rounded w-25 border loginForm'>
             <h2>Login page</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='mb-3'>
                     <label htmlFor='email'><strong>Email</strong></label>
-                    <input type='email' name='email' autoComplete='off' placeholder='Enter Email' className='form-control rounded-0' />
+                    <input type='email' name='email' autoComplete='off' placeholder='Enter Email' onChange={(e)=> setValues({...values, email : e.target.value})} className='form-control rounded-0' />
                 </div>
                 <div className='mb-3'>
                     <label htmlFor='password'><strong>Password</strong></label>
-                    <input type='password' name='password'  placeholder='Enter password' className='form-control rounded-0' />
+                    <input type='password' name='password'  placeholder='Enter password'  onChange={(e)=> setValues({...values, password : e.target.value})}  className='form-control rounded-0' />
                 </div>
                 <button className='btn btn-success w-100 rounded-0 mb-2'>Login</button>
                 <div className='mb-1'>
