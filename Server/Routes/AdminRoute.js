becf89da764de497bc5222c01d23244f9107b785
Bisrat Tamire <biarattamire288@gmail.com>
2025-02-12 22:17:32 +0300

    connected

---
+++ Server/Routes/AdminRoute.js
@@ -0,0 +1,12 @@
+// all the apis
+
+import express from "express";
+
+const router = express.Router();
+
+router.post("/adminlogin", (req, res) => {
+  console.log(req.body);
+});
+
+
+export {router as adminRouter}
