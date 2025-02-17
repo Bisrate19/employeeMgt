d46225ced78e93b8133183eb62b9ad5c6a7fa6db
Bisrat Tamire <biarattamire288@gmail.com>
2025-02-18 00:27:58 +0300

    home

--- EmployeeMgt/src/App.jsx
+++ EmployeeMgt/src/App.jsx
@@ -1,21 +1,21 @@
-import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css"
-import Login from './Components/Login'
-import Dashboard from './Components/Dashboard';
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dashboard";
+import Home from "./Components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path='/adminlogin' element={<Login/>}></Route>
-    <Route path='/dashboard' element={<Dashboard/>}></Route>
-   </Routes>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/adminlogin" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="" element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
