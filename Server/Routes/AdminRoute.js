7321d5c8e8e08473db2a5aa875201bbad83b959d
Bisrat Tamire <biarattamire288@gmail.com>
2025-02-18 18:58:34 +0300

    display category

--- Server/Routes/AdminRoute.js
+++ Server/Routes/AdminRoute.js
@@ -1,46 +1,42 @@
-// all the apis
-
 import express from "express";
-import con from "../utils/db.js"
-import jwt from "jsonwebtoken"
-
-
-
-
+import con from "../utils/db.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post("/adminlogin", (req, res) => {
-const sql = "select * from admin where email = ? and password = ?"
-con.query(sql,[req.body.email, req.body.password], (err,result) =>{
-    if (err) return res.json({loginStatus: false, Error: "Query error"})
-        if(result.length > 0){
-            const email = result[0].email;
-            const token = jwt.sign(
-                {role: "admin", email:email},
-                "jwt_secret_key",
-                {expiresIn : "1d"}
-            )
-res.cookie('token', token)
-return res.json({loginStatus: true })
-         
+  const sql = "select * from admin where email = ? and password = ?";
+  con.query(sql, [req.body.email, req.body.password], (err, result) => {
+    if (err) return res.json({ loginStatus: false, Error: "Query error" });
+    if (result.length > 0) {
+      const email = result[0].email;
+      const token = jwt.sign(
+        { role: "admin", email: email },
+        "jwt_secret_key",
+        { expiresIn: "1d" }
+      );
+      res.cookie("token", token);
+      return res.json({ loginStatus: true });
+    } else {
+      return res.json({ loginStatus: false, Error: "wrong email or password" });
+    }
+  });
+});
 
-} else {
-    return res.json({loginStatus: false, Error: "wrong email or password"})
-}
-} )
-
-
-
+router.get("/category", (req, res) => {
+  const sql = "select * from category";
+  con.query(sql, (err, result) => {
+    if (err) return res.json({ Status: false, Error: "Query error" });
+    return res.json({ Status: true, Result: result });
+  });
 });
 
 router.post("/add_category", (req, res) => {
-    const sql = "insert into category (name) values (?)"
-    con.query(sql, [req.body.category], (err, result) => {
-        if (err) return res.json({Status: false, Error: "Query error"})
-        return res.json({Status: true})
-    })
-})
+  const sql = "insert into category (name) values (?)";
+  con.query(sql, [req.body.category], (err, result) => {
+    if (err) return res.json({ Status: false, Error: "Query error" });
+    return res.json({ Status: true });
+  });
+});
 
-
-export {router as adminRouter}
+export { router as adminRouter };
